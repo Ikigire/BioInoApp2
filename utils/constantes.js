f7b98a1540035598b5ebc0d1b0a0b00d7ae31674
Yael Alejandro Santana Michel <51036254+Ikigire@users.noreply.github.com>
2024-01-15 01:03:30 -0600

    Listo para entrega

--- utils/constantes.js
+++ utils/constantes.js
@@ -1,6 +1,6 @@
 
-// export const baseUrl = "http://3.19.28.164:3000";
-export const baseUrl = "http://192.168.1.111";
+export const baseUrl = "http://3.19.28.164:3000";
+// export const baseUrl = "http://192.168.1.111";
 
 export const Content_Type = 'application/json';
 
